38264a2e70aef2fe7d8da8f4d82babededc2cd26
Ramson <ramson.odoi@amalitech.org>
2024-04-19 17:50:05 +0000

    feat: redesign-all-products-page

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product, TableProducts } from 'src/app/core/models/interfaces/productInterface';
+import { ProductsService } from 'src/app/core/service/products/products.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  public products: TableProducts[] = [];
+  public constructor(private product: ProductsService){
+  }
+  public ngOnInit(): void {
+    // this.getAllProducts();
+    this.product.getAllProducts().subscribe(
+      (data) => {
+        this.products =data.normalizedProductsDetails;
+        console.log(this.products);
+      }
+    );
+  }
+  
+
+  // public getAllProducts() {
+  //   this.product.getAllProducts().subscribe(
+  //     (data) => {
+  //       this.products =data;
+  //     },
+  //     (error)=> {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
+
 
 }
